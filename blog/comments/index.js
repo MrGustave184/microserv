e54c40644184eb73e64d1af2fcb6058f449f1d6b
MrGustave184 <rafaeldavid184@gmail.com>
2021-10-17 11:26:36 -0400

    setup comments service

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -5,3 +5,25 @@ const app = express();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+    const commentId = randomBytes(4).toString('hex');
+    const { content } = req.body;
+
+    const comments = commentsByPostId[req.params.id] || [];
+    comments.push({ id: commentId, content });
+
+    commentsByPostId[req.params.id] = comments;
+
+    res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+    console.log("Comments service listening on port 4001");
+})
